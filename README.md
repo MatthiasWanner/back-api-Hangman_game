5c52295fd54e789807fb39faf9cedc19a055ed3a
MatthiasWanner <matthias.wanner@gmail.com>
2021-04-04 20:38:25 +0200

    Add instructions in README.me file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-#Express API
+#Express API  
 
-Little API to serve words in my Hangman Game : https://github.com/MatthiasWanner/vite-hangman_game
+Little API to serve words in my Hangman Game : https://github.com/MatthiasWanner/vite-hangman_game  
+
+Run "npm start" in this repo to launch server  
+Run "npm run dev" in the front repo
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { restart } = require('nodemon');
-const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, cache-control');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next(); // dont' forget the next function to continue middlewares of your Express Applications
+});
+
+const PORT = process.env.PORT || 5000;
 const WORDSAPI = require('./wordsapi_sample');
 const REGEX = /[^a-z]+/i;
+
 // function that return an array of global object in JSON file, and add a proprity "word" which countain the word in upper string
 const exploitObject = (object) => {
   const keys = Object.keys(object);
@@ -22,13 +31,6 @@ const filterByLength = (object, value) => {
   const wordsArrayFiltered = wordsArray.filter((word) => word.letters === value && !REGEX.test(word.word));
   return wordsArrayFiltered;
 }
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next(); // dont' forget the next function to continue middlewares of your Express Applications
-});
 
 app.get('/api/wordsapi', (req, res) => {
   res.status(200).json(exploitObject(WORDSAPI));
